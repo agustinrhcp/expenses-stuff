9db13bac4d525b1583e622f9ecd5b9feba79a668
Agustin Cornu <agustinrhcp@gmail.com>
2015-11-12 22:35:28 -0300

    Add expense validations.

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,10 @@
 class Expense < ActiveRecord::Base
+
+	validates :amount, numericality: {
+    greater_than: 0,
+    message: 'InvalidAmount - Amount attributes must be greater than zero'
+  }
+	validates :description, presence: {
+		message: 'InvalidDescription - Description can\'t be blank'
+	}
 end
---
+++ spec/models/expense_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe Expense do
+	describe 'validations' do
+		context 'when the amount is not a number' do
+			let(:amount) { 'two' }
+
+			it 'fails' do
+        expect {
+          FactoryGirl.create(:expense, amount: amount)
+        }.to raise_error(ActiveRecord::RecordInvalid, /InvalidAmount/)
+      end
+		end
+
+		context 'when the amount is less than zero' do
+			let(:amount) { -100 }
+
+			it 'fails' do
+        expect {
+          FactoryGirl.create(:expense, amount: amount)
+        }.to raise_error(ActiveRecord::RecordInvalid, /InvalidAmount/)
+      end
+		end
+
+		context 'when the description is blank' do
+			let(:description) { nil }
+
+			it 'fails' do
+        expect {
+          FactoryGirl.create(:expense, description: description)
+        }.to raise_error(ActiveRecord::RecordInvalid, /InvalidDescription/)
+      end
+		end
+	end
+end
