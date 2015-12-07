949206a3fdaf271bb890ba528187534d56fbb600
Agustin Cornu <agustinrhcp@gmail.com>
2015-12-06 22:15:22 -0300

    Add monthly expenses.

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -4,11 +4,17 @@ class Expense < ActiveRecord::Base
   validates :amount, numericality: { greater_than: 0 }
   validates :description, presence: true
 
+  scope :applies_monthly?, -> { where(applies_monthly: true) }
+  scope :created_at, ->(date) do
+    where(created_at: date.beginning_of_month..date.end_of_month)
+  end
+
   def self.total
     sum(:amount)
   end
 
   def self.by_date(date)
-    where(created_at: date.beginning_of_month..date.end_of_month)
+    # v HACK .or.where() does not exist yet :(
+    (applies_monthly? + created_at(date)).uniq
   end
 end
